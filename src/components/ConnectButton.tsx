82aa0a8fc802129fa11bdce0e5dd97e022e15e29
Ayush Bherwani <ayush.bherwani1998@gmail.com>
2025-12-05 08:53:16 +0400

    Update example
    * update example
    * update next js version

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { useConnect } from "wagmi";
+import { useAccount, useChainId, useConnect, useSwitchChain } from "wagmi";
 import Button from "@/components/Button";
 import { metaMask } from "wagmi/connectors";
 
 export default function ConnectButton() {
     const { connect } = useConnect();
+    const { chainId: connectedChainId, isConnected } = useAccount();
+    const { switchChain } = useSwitchChain();
+    const currentChainId = useChainId();
+
+    if (isConnected && connectedChainId !== currentChainId) {
+        return (
+            <Button className="w-full space-x-2" onClick={() => switchChain({ chainId: currentChainId })}>
+                <span>Switch Chain</span>
+            </Button>
+        );
+    }
 
     return (
         <Button className="w-full space-x-2" onClick={() => connect({ connector: metaMask() })}>
